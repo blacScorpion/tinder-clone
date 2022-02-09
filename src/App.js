8381b99c695bb49979e5237c8e1d29c64f6e0d0e
scorpionking <atofelixessien@hotmail.com>
2022-02-09 07:04:52 +0000

    Built TinderCards using react tinder

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Header from "./Header";
+import TinderCards from "./TinderCards";
 
 function App() {
   return (
     <div className="App">
-
+      {/* Header component */}
       <Header />
-
+      {/* Tinder Cards */}
+      <TinderCards />
     </div>
   );
 }
---
+++ src/TinderCards.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
+import "./TinderCards.css";
+function TinderCards() {
+  const [people, setPeople] = useState([
+    {
+      name: "Elon Musk",
+      url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+    },
+    {
+      name: "Jeff Bezos",
+      url: "https://www.wired.com/wp-content/uploads/2019/01/Culture_GeeksGuide_Bezos.jpg",
+    },
+  ]);
+  const swiped = (direction, nameToDelete) => {
+    console.log("removing: " + nameToDelete);
+  };
+  const outOfFrame = (name) => {
+    console.log(name + " left the screen");
+  };
+  return (
+    <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
+        {people.map((person) => (
+          <TinderCard
+            className="swipe"
+            key={person.name}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          >
+            <div
+              style={{ backgroundImage: `url(${person.url})` }}
+              className="card"
+            >
+              <h3>{person.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TinderCards;
